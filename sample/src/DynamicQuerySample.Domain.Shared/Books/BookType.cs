3d6aa1e9903844c21d12bc5fe49ad067331775ec
wakuflair <wakuflair@163.com>
2020-09-18 16:43:33 +0800

    add Book entity

---
+++ sample/src/DynamicQuerySample.Domain.Shared/Books/BookType.cs
@@ -0,0 +1,15 @@
+namespace DynamicQuerySample.Books
+{
+    public enum BookType
+    {
+        Undefined,
+        Adventure,
+        Biography,
+        Dystopia,
+        Fantastic,
+        Horror,
+        Science,
+        ScienceFiction,
+        Poetry
+    }
+}
