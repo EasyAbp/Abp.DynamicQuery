8660bb956e7172b58f6c6b9deaa5b915c5eb0dc3
gdlcf88 <gdlcf88@outlook.com>
2024-07-25 18:37:11 +0800

    Upgrade to ABP 8.2.0

--- test/EasyAbp.Abp.DynamicQuery.EntityFrameworkCore.Tests/EntityFrameworkCore/DynamicQueryEntityFrameworkCoreTestModule.cs
+++ test/EasyAbp.Abp.DynamicQuery.EntityFrameworkCore.Tests/EntityFrameworkCore/DynamicQueryEntityFrameworkCoreTestModule.cs
@@ -30,7 +30,7 @@ namespace EasyAbp.Abp.DynamicQuery.EntityFrameworkCore
         
         private static SqliteConnection CreateDatabaseAndGetConnection()
         {
-            var connection = new SqliteConnection("Data Source=:memory:");
+            var connection = new AbpUnitTestSqliteConnection("Data Source=:memory:");
             connection.Open();
 
             return connection;
