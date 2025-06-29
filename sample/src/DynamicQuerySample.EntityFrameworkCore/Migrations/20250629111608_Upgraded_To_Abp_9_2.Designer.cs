e96c43431e64c19caa5a25c4852df2f99e0415af
gdlcf88 <gdlcf88@outlook.com>
2025-06-29 19:16:44 +0800

    Add EF migration for ABP v9.2

--- sample/src/DynamicQuerySample.EntityFrameworkCore/Migrations/20250629111608_Upgraded_To_Abp_9_2.Designer.cs
+++ sample/src/DynamicQuerySample.EntityFrameworkCore/Migrations/20250629111608_Upgraded_To_Abp_9_2.Designer.cs
@@ -4,6 +4,7 @@ using DynamicQuerySample.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace DynamicQuerySample.Migrations
 {
     [DbContext(typeof(DynamicQuerySampleDbContext))]
-    partial class DynamicQuerySampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250629111608_Upgraded_To_Abp_9_2")]
+    partial class Upgraded_To_Abp_9_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
